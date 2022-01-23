d97f67d1435a64ff6bcd6f35e954aeeb46c00bab
Andrey Mats <amats6655@gmail.com>
2022-01-23 23:08:42 +0300

    first

---
+++ app/src/main/java/ru/mrkotik/kalc/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mrkotik.kalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
